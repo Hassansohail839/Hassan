bb493ec381990051b16640b72c5ea0bbba75869a
Hassan <hassansohail839@gmail.com>
2025-02-17 13:38:48 +0100

    Tarea 4

---
+++ Variables Y constantes/src/ejercicios/Ejercicio1App.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class Ejercicio1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              int a=10;
+              int b=20;
+              int suma=a+b;
+              System.out.print("La suma de a + b es " + suma);
+
+	}
+
+}
---
+++ Variables Y constantes/src/ejercicios/Ejercicio3App.java
@@ -0,0 +1,44 @@
+package ejercicios;
+
+public class Ejercicio3App {
+
+	public static void main(String[] args) {
+		//Definición de variables
+		int X = 10;
+		int Y = 20;
+		double N = 5.5;
+		double M = 8.5;
+		  System.out.println("Variable X = 10");
+          System.out.println("Variable Y = 20");
+          System.out.println("Variable N = 5.5");
+          System.out.println("Variable M = 8.5");
+		
+		//Programo las operaciones y le doy una explicación para saber de donde viene el resultado.
+		 double add= X+Y;
+		 double substract=X-Y;
+		System.out.println("La Suma de X + Y es "+add);
+		System.out.println("La resta de X - Y es "+substract);
+		double multiply= X*Y;
+		System.out.println("El producto de X * Y es "+multiply);
+//		double divide=X/Y;
+		System.out.println("El cociente de X / Y es "+X/Y);
+		double percentage=X%Y;
+		System.out.println("La resta de X % Y es "+percentage);
+		double add2=N+M;
+		System.out.println("La Suma de N + M es "+add2);
+		double substract2=N-M;
+		System.out.println("La resta de N - M es "+substract2);
+	    double multiply2=N*M;
+	    System.out.println("El producto de N * M es "+multiply2);
+		double divide2=N/M;
+		System.out.println("El cociente de N / M es "+divide2);
+		double percentage2=N%M;
+		System.out.println("La resta de N % M es "+percentage2);
+		double add3=X+N;
+		System.out.println("La suma de X + N es "+add3);
+//		double 
+		
+		
+	}
+
+}
