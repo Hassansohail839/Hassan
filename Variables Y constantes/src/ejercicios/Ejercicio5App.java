9037bbcf17f89c148dc94fa12b1ce523de273062
Hassan <hassansohail839@gmail.com>
2025-02-18 13:57:15 +0100

    Update Ejercicio5App.java

--- Variables Y constantes/src/ejercicios/Ejercicio5App.java
+++ Variables Y constantes/src/ejercicios/Ejercicio5App.java
@@ -10,15 +10,17 @@ public class Ejercicio5App {
 		int B1 = B;
 		int C = 15;
 		int D = 20;
-		System.out.println("Valor de A es " + A);
-		System.out.println("Valor de B es " + B);
-		System.out.println("Valor de C es " + C);
-		System.out.println("Valor de D es " + D);
+		System.out.println("Valor de A es " + A); //5
+		System.out.println("Valor de B es " + B); //10
+		System.out.println("Valor de C es " + C); //15
+		System.out.println("Valor de D es " + D); //20
+//		B1 contiene 10
 
-		System.out.println("B tome el valor de C = " + (B = C));
-		System.out.println("C tome el valor de A = " + (C = A));
-		System.out.println("A tome el valor de D = " + (A = D));
-		System.out.println("D tome el valor de B = " + (D = B1));
+		System.out.println("B tome el valor de C = " + (B = C)); // 15
+		System.out.println("C tome el valor de A = " + (C = A)); // 5
+		System.out.println("A tome el valor de D = " + (A = D)); // 20
+//		B1 contiene 10	
+		System.out.println("D tome el valor de B = " + (D = B1)); // 10
 
 	}
 
