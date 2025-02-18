35127ab4cb7bc18e0142453abeebd994cf6d8899
Hassan <hassansohail839@gmail.com>
2025-02-18 12:25:15 +0100

    Create Ejercicio4App.java

---
+++ Variables Y constantes/src/ejercicios/Ejercicio4App.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+public class Ejercicio4App {
+
+	 public static void main(String[] args) {
+	       
+	        int N = 10;
+
+	       System.out.println("Valor de N = " + N);
+
+	         N += 77;
+	        System.out.println("(N + 77) = " + N);
+
+	        N -= 3;
+	        System.out.println("(N - 3) = " + N);
+
+	        N *= 2;
+	        System.out.println("(N * 2) = " + N);
+	    }
+	}
