ab793e352ef5f0433eb099188d8142801fa70f46
Hassan <hassansohail839@gmail.com>
2025-02-21 12:36:37 +0100

    Ok

--- Flujo de datos/src/Ejercicio2App.java
+++ Flujo de datos/src/Ejercicio2App.java
@@ -4,7 +4,7 @@ public class Ejercicio2App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String nombre=JOptionPane.showInputDialog("Introduce tu nombre");
+String nombre=JOptionPane.showInputDialog("Hassan ");
 JOptionPane.showMessageDialog(null, "Bienvenido "+nombre);
 	}
 
