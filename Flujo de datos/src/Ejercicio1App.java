8650f68c07b3e7d75603df3e543bb4dee16d837f
Hassan <hassansohail839@gmail.com>
2025-02-21 12:27:29 +0100

    OK

---
+++ Flujo de datos/src/Ejercicio1App.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio1App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int A = 80;
+		int B = 50;
+		
+		if(A > B) {
+			System.out.println( "Variable A, " + A + " es mayor que variable B, " + B);
+		} else if (A < B) {
+			System.out.println("Variable B, " + B + " es mayor que variable A, " + A);
+		} else {
+			System.out.println("Variable A, " +A + " y variable B, " + B + " son iguales");
+		}
+
+	}
+
+}
