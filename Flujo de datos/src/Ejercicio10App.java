a916cb46a9c2b69f35bcc7865df6911618e0a2eb
Hassan <hassansohail839@gmail.com>
2025-02-24 13:00:31 +0100

    Update Ejercicio10App.java

--- Flujo de datos/src/Ejercicio10App.java
+++ Flujo de datos/src/Ejercicio10App.java
@@ -2,7 +2,24 @@
 public class Ejercicio10App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Realiza una aplicación que nos pida un número de ventas a introducir. 
+		//Después nos pedirá tantas ventas por teclado como número de ventas se hayan indicado. 
+		//Al final mostrara la suma de todas las ventas. Piensa que es lo que se repite y lo que no.
+		
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Introduce el número de ventas a introducir");
+		int numeroVentas = sc.nextInt();
+		int sumaVentas = 0;
+		for (int i = 1; i <= numeroVentas; i++) {
+			System.out.println("Introduce la venta " + i);
+			int venta = sc.nextInt();
+			sumaVentas += venta;
+			System.out.println("La suma de las ventas es " + sumaVentas);
+	
+		}
+		
+		
+	
 
 	}
 
