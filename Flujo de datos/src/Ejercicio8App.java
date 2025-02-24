ac9890d1c37d0b236f9d21a77fd3c5fee5f66f1e
Hassan <hassansohail839@gmail.com>
2025-02-24 12:41:57 +0100

    OK

---
+++ Flujo de datos/src/Ejercicio8App.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio8App {
+
+	public static void main(String[] args) {
+		//Muestra los números del 1 al 100 (ambos incluidos).
+		//Usa un bucle for.
+		
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
