0240b5b9e60f6011480f7026d2a1b0074bccdb6f
Hassan <hassansohail839@gmail.com>
2025-02-24 12:40:04 +0100

    OK

---
+++ Flujo de datos/src/Ejercicio7App.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio7App {
+
+	public static void main(String[] args) {
+		//Muestra los números del 1 al 100 (ambos incluidos). 
+		//Usa un bucle while.
+		
+		int i = 1;
+		while (i <= 100) {
+			System.out.println(i);
+			i++;
+	
+        }		
+		}
+	
+	}
