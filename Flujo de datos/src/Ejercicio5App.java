b0bb6d7dd3b97cb182f8e334c861f484fd9ea52c
Hassan <hassansohail839@gmail.com>
2025-02-24 13:21:13 +0100

    ok

--- Flujo de datos/src/Ejercicio5App.java
+++ Flujo de datos/src/Ejercicio5App.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio5App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Introduce un numero entero");
 		int numero = sc.nextInt();
 
@@ -12,10 +15,11 @@ public class Ejercicio5App {
 
 		if (numero % 2 == 0) {
 			System.out.println("El número " + numero + " es divisible entre 2.");
+			
 		} else {
 			System.out.println("El número " + numero + " no es divisible entre 2.");
+			
 		}
-
 		sc.close();
 
 	}
