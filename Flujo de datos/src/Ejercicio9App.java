2553d11b8fcb429e5a602c0a47142c51a478c705
Hassan <hassansohail839@gmail.com>
2025-02-24 12:52:07 +0100

    OK

---
+++ Flujo de datos/src/Ejercicio9App.java
@@ -0,0 +1,22 @@
+
+public class Ejercicio9App {
+
+	public static void main(String[] args) {
+		// Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. 
+		//Utiliza el bucle que desees.
+		
+		int i = 1;
+		while (i <= 100) {
+			if (i % 2 == 0 && i % 3 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+		
+
+
+	
+
+}
+	
+	}
