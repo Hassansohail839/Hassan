a486e48f048dd87f0e8357fcae245ebf1f51040a
Hassan <hassansohail839@gmail.com>
2025-03-09 17:35:58 +0100

    OK

---
+++ Métodos y Arrays/src/Ejercicio12App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio12App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el tamaño del array: ");
+        int size = scanner.nextInt();
+        int range = 300;
+        
+        System.out.print("Introduce el ultimo valor: ");
+        int ultimoValor=scanner.nextInt();
+        
+        
+       // Metodo para generar un array aleatorio.
+        
+       
+        
+        
+      
+
+       
+	}
+
+}
