235dc413cc4c7639673e7231f5333fea2011b88c
Hassan <hassansohail839@gmail.com>
2025-03-17 10:51:45 +0100

    OKAY

---
+++ Métodos y Arrays/src/Ejercicio07App.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Ejercicio07App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Introduce la cantidad de Euros(€) que deseas cambiar: ");
+		double cantidadEuros=sc.nextInt();
+		System.out.println("Introduce la moneda de cambio: ");
+		System.out.println("1) dolares".toLowerCase());
+		System.out.println("2) yenes".toLowerCase());
+		System.out.println("3) libras".toLowerCase());
+		String moneda=sc.next();
+		cambioMoneda(cantidadEuros, moneda);
+			
+		}
+		
+		
+	
+    public static void cambioMoneda(double cantidadEuros, String moneda) {
+    	double resultado=0.0;
+    	if (moneda.equals("libras")) {
+            resultado = cantidadEuros * 0.86;
+            System.out.println(cantidadEuros + "€ son " + resultado + "£.");
+        } else if (moneda.equals("dolares")) {
+            resultado = cantidadEuros * 1.28611;
+            System.out.println(cantidadEuros + "€ son " + resultado + "$.");
+        } else if (moneda.equals("yenes")) {
+            resultado = cantidadEuros * 129.852;
+            System.out.println(cantidadEuros + "€ son " + resultado + "¥.");
+        } else {
+          
+            System.out.println("Moneda no válida. Por favor elige entre 'libras', 'dolares' o 'yenes'.");
+        }
+	
+	
+	
+}
+	
+}
