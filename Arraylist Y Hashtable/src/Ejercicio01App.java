1be781efad329a8a0d04fb66c790303e19b70f60
Hassan <hassansohail839@gmail.com>
2025-03-18 10:55:18 +0100

    OKAY

---
+++ Arraylist Y Hashtable/src/Ejercicio01App.java
@@ -0,0 +1,58 @@
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class Ejercicio01App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		//Crear un hashtable para guardar el nombre y las notas de cada alumno.
+		Hashtable<String, Double>notasAlumnos= new Hashtable<>();
+		
+		//Pedir el numero de alumnos.
+		System.out.println("Introduce el numero de alumonos: ");
+		int numAlumnos=sc.nextInt();
+		// Bucle para ingresar los datos de cada alumno.
+		 for (int i = 0; i < numAlumnos; i++) {
+	            // Pedir el nombre del alumno
+	            System.out.print("Introduce el nombre del alumno: ");
+	            String nombre = sc.next();
+	            
+	            // Pedir numero de preguntas.
+	            System.out.println("Introduce el numero de asiganturas: ");
+	            int numAsignaturas=sc.nextInt();
+	            
+	            // Pedir las calificaciones de las asignaturas
+	            double sumaNotas = 0;
+	            for (int j = 0; j < numAsignaturas; j++) {
+	                System.out.print("Introduce la calificación de la asignatura " + (j + 1) + ": ");
+	                sumaNotas += sc.nextDouble();
+	            }
+
+	         // Calcular la nota media
+	            double notaMedia = sumaNotas / numAsignaturas;
+	            
+	         // Almacenar el nombre y la nota media en el Hashtable
+	            notasAlumnos.put(nombre, notaMedia);
+	        }
+		
+		// Mostrar las notas medias de los alumnos
+	        System.out.println("\nNotas medias de los alumnos:");
+	        for (String nombre : notasAlumnos.keySet()) {
+	            System.out.println("Alumno: " + nombre + " - Nota media: " + notasAlumnos.get(nombre));
+	        }
+
+	        // Cerrar el scanner
+	        sc.close();
+	    }
+	
+		
+		
+
+		
+	}
+
+	
+	
+
