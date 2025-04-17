7e586aafc0552863a633226f03689b0ce36c896f
Hassan <hassansohail839@gmail.com>
2025-04-17 15:07:47 +0200

    Create Serie.java

---
+++ Herencia/src/Ejercicio02/Serie.java
@@ -0,0 +1,22 @@
+package Ejercicio02;
+
+public class Serie {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+
+	// Atributos
+	protected String titulo;
+	protected int numeroTemporadas;
+	protected boolean entregado;
+
+	protected final static int NUMEROTEMPORADAS_DEFECTO = 3;
+	protected final boolean ENTREGADO_DEFECTO = false;
+
+	
+	//Constructor por defecto
+	
+	public Serie() {
+		this.titulo=
+	}
+}
