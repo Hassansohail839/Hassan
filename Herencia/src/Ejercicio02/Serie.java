57c4c17e5cbd51b0900987a87b2025ea4823788c
Hassan <hassansohail839@gmail.com>
2025-04-23 12:56:37 +0200

    OKAY

--- Herencia/src/Ejercicio02/Serie.java
+++ Herencia/src/Ejercicio02/Serie.java
@@ -9,14 +9,85 @@ public class Serie {
 	protected String titulo;
 	protected int numeroTemporadas;
 	protected boolean entregado;
+	protected String genero;
+	protected String creador;
 
 	protected final static int NUMEROTEMPORADAS_DEFECTO = 3;
 	protected final boolean ENTREGADO_DEFECTO = false;
+	protected final String GENERO_DEFECTO = "";
 
-	
-	//Constructor por defecto
-	
+	// Constructor por defecto
+
 	public Serie() {
-		this.titulo=
+		this.titulo = "";
+		this.numeroTemporadas = 3;
+		this.entregado = false;
+		this.genero = "";
+		this.creador = "";
+
 	}
+
+	// Constructor con titulo y creador
+
+	public Serie(String titulo, String creador) {
+		this.titulo = titulo;
+		this.creador = creador;
+		this.numeroTemporadas = NUMEROTEMPORADAS_DEFECTO;
+		this.entregado = ENTREGADO_DEFECTO;
+		this.genero = GENERO_DEFECTO;
+
+	}
+
+	// Constructor con todos los atributos excepto el entregado
+
+	public Serie(String titulo, String creador, int numeroTemporadas, String genero) {
+
+		this.titulo = titulo;
+		this.creador = creador;
+		this.numeroTemporadas = NUMEROTEMPORADAS_DEFECTO;
+		this.genero = GENERO_DEFECTO;
+
+	}
+
+	// Getters y Setters, excepto el entregado
+
+	public String gettitulo() {
+		return titulo;
+	}
+
+	public void settitulo(String titulo) {
+		this.titulo = titulo;
+
+	}
+
+	public int getnumeroTemporadas() {
+		return numeroTemporadas;
+	}
+
+	public void setnumeroTemporadas(int numeroTemporadas) {
+		this.numeroTemporadas = numeroTemporadas;
+
+	}
+
+	public String getgenero() {
+		return genero;
+
+	}
+
+	public void setgenero(String genero) {
+		this.genero = genero;
+
+	}
+
+	public String getcreador() {
+		return genero;
+
+	}
+
+	public void setcreador(String creador) {
+		this.creador = creador;
+
+	}
+	
+	
 }
---
+++ Herencia/src/Ejercicio02/Videojuego.java
@@ -0,0 +1,71 @@
+package Ejercicio02;
+
+public class Videojuego {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+
+	// Atributos
+
+	private String titulo;
+	private int horasEstimadas;
+	private String compania;
+	private boolean entregado;
+
+	private final int HORASESTIMADAS_DEFECTO = 10;
+	private final boolean ENTREGADO_DEFECTO = false;
+
+	// Constructor por defecto
+	public Videojuego() {
+		super();
+		this.horasEstimadas = HORASESTIMADAS_DEFECTO;
+		this.entregado = ENTREGADO_DEFECTO;
+		this.compania = compania;
+	}
+
+	// Constructor con titulo y creador
+
+	public Videojuego(String titulo, int horasEstimadas) {
+		super();
+		this.horasEstimadas = HORASESTIMADAS_DEFECTO;
+		this.entregado = ENTREGADO_DEFECTO;
+		this.compania = compania;
+	}
+
+	// Constructor con todos los atributos menos el entregado
+
+	public Videojuego(String titulo, int numeroTemporadas, 
+			boolean entregado, String genero, String creador,
+			int horasEstimadas, String compania) {
+		
+		this.horasEstimadas = horasEstimadas;
+		this.entregado = entregado;
+		this.compania = compania;
+
+	}
+
+	
+	// Getters y Setters 
+	
+	public int gethorasEstimadas() {
+		return horasEstimadas;
+	}
+
+	public void sethorasEstimadas(int horasEstimadas) {
+		this.horasEstimadas = horasEstimadas;
+	}
+	
+	
+	public String getcompania() {
+		return compania;
+		
+	}
+
+	public void setcompania(String compania) {
+		this.compania=compania;
+		
+	}
+	
+}
