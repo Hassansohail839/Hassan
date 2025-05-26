53887b20a3b55ad4dab77e5d34e26515b65a7e71
Hassan <hassansohail839@gmail.com>
2025-05-26 12:19:02 +0200

    OK

---
+++ UD14/EJE03.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE tiendainfo;
+USE tiendainfo;
+
+CREATE TABLE fabricantes (
+    codigo INT,
+    nombre VARCHAR(100),
+    PRIMARY KEY (codigo));
+
+ALTER TABLE `fabricantes` CHANGE `codigo` `codigo` INT(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `fabricantes` (`codigo`, `nombre`) VALUES (NULL, 'HP');
+INSERT INTO fabricantes (nombre) VALUES ('LG');
+
+
+CREATE TABLE articulos (
+    codart INT  NOT NULL  AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    precio FLOAT,
+    fabricante INT,
+    PRIMARY KEY (codart),
+    FOREIGN KEY (fabricante) 
+    REFERENCES fabricantes (codigo) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE);
+
+
+
+CREATE TABLE `tiendainfo`.`prova` (`id` INT NOT NULL AUTO_INCREMENT , `nombre` VARCHAR(60) NOT NULL , PRIMARY KEY (`id`));
