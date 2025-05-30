b9c5aa170e51979a8d35d858959d91dec5ebf6af
Hassan <hassansohail839@gmail.com>
2025-05-30 13:17:15 +0200

    OKAY

---
+++ ventanaGrafica/src/main/java/ventanaGrafica/VentanaApp.java
@@ -0,0 +1,25 @@
+package ventanaGrafica;
+
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.EventQueue;
+
+public class VentanaApp {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame("Mi Ventana Gráfica");
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Salir al cerrar
+            frame.setSize(400, 300); // Tamaño inicial
+//            frame.setResizable(true); // Permite cambiar tamaño
+            frame.setLocationRelativeTo(null); // Centrar ventana
+
+            JLabel etiqueta = new JLabel("Hola, esta es una etiqueta", JLabel.CENTER);
+            frame.add(etiqueta);
+
+            frame.setVisible(true);
+        });
+    }
+}
